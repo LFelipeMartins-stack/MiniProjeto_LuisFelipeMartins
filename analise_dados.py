00b68162276a98304b0ce4165c7014d098382659
lFelipeMar <L.Felipe.Martins@hotmail.com>
2026-05-31 02:30:01 -0300

    Incluindo amostra de dados e identificando nulos...

--- analise_dados.py
+++ analise_dados.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-# ETAPA 1: Carregar a base e exibir informações estruturais
-# __________________________________________________________________
+# ETAPA 1: Carregar a base e exibir informações estruturais -> Início
+# ______________________________________________________________________________________
 
-print("--- INICIANDO A SPRINT 1: IMPORTAÇÃO DOS DADOS ---")
+print("--- INICIANDO A SPRINT 1: IMPORTAÇÃO E ENTENDIMENTO DOS DADOS ---")
 
 # Carregando a base de dados com pandas
 # Usando sep=';' pois é o padrão da base Varejo
 caminho_arquivo = 'Base Varejo.csv'
 df = pd.read_csv(caminho_arquivo, sep=';')
 
-# Extraindo o número de registros (linhas) e colunas
+# Extraindo o número de registros (linhas) e colunas para visualizar estrutura do dataframe
 num_registros = df.shape[0]
 num_colunas = df.shape[1]
 
@@ -21,3 +21,16 @@ print(f"\n3. NOME DAS COLUNAS:")
 print(list(df.columns))
 print(f"\n4. TIPOS DE DADOS POR COLUNA:")
 print(df.dtypes)
+
+# Amostra inicial dos dados (df.head)
+print("\nAMOSTRA DOS DADOS (df.head()):")
+print(df.head())
+
+# Verificação inicial de valores nulos (df.isnull().sum())
+print("\nVALORES NULOS POR COLUNA (df.isnull().sum()):")
+nulos = df.isnull().sum()
+print(nulos[nulos > 0]) # Filtra para exibir apenas colunas que possuem nulos
+
+print("--- FINALIZANDO A SPRINT 1: IMPORTAÇÃO E ENTENDIMENTO DOS DADOS ---")
+# ______________________________________________________________________________________
+# ETAPA 1: Carregar a base e exibir informações estruturais -> Fim
